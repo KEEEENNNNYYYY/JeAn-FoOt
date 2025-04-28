4293946531c3509b3c18c219a28ed7672d5a9c79
nonieKley <hei.oninaina@gmail.com>
2025-04-28 15:42:34 +0300

    feat(RestControllers) : add Controller for Clubs

--- src/main/java/com/example/demo/RestControllers/ClubController.java
+++ src/main/java/com/example/demo/RestControllers/ClubController.java
@@ -1,4 +1,31 @@
 package com.example.demo.RestControllers;
 
+
+import com.example.demo.DTO.ClubDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clubs")
+@RequiredArgsConstructor
 public class ClubController {
+
+    private final ClubService service;
+
+    @GetMapping
+    public ResponseEntity<List<ClubDTO>> getAll(){
+        List<ClubDTO> clubs = service.findAll();
+        return ResponseEntity.ok(clubs);
+    }
+
+    @GetMapping
+    public ResponseEntity<ClubDTO> getById(){
+        ClubDTO club = service.findById();
+        return ResponseEntity.ok(club);
+    }
 }
