bc4e235b7bf5b1cf2a431597ce483ca7647bec2b
nonieKley <hei.oninaina@gmail.com>
2025-04-28 15:41:42 +0300

    feat(DTO): add ClubDTO

---
+++ src/main/java/com/example/demo/DTO/ClubDTO.java
@@ -0,0 +1,19 @@
+package com.example.demo.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ClubDTO {
+    private String name;
+    private LocalDate createDate;
+    private String acronym;
+    private String country_id; // MDG pour madagascar ohatra ou jsp
+}
