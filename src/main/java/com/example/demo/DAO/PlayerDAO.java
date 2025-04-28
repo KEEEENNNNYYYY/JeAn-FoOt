976c80b7532d87b472bee8f303684fd1446e5915
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-04-28 16:25:50 +0200

    chore : initialization of player DAO

---
+++ src/main/java/com/example/demo/DAO/PlayerDAO.java
@@ -0,0 +1,19 @@
+package com.example.demo.DAO;
+
+import com.example.demo.Entities.Player;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
+public class PlayerDAO {
+
+    public List<Player> findAll(){
+        List<Player> playerList = new ArrayList<>();
+        System.out.println("not implemented yet");
+        return playerList;
+    }
+}
