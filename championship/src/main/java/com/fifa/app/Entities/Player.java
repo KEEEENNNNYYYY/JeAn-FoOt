09c5ee0edd5e2cfd046b550cdb1996545b4b2994
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-04-29 16:28:12 +0200

    fix : wrong attribut

--- championship/src/main/java/com/fifa/app/Entities/Player.java
+++ championship/src/main/java/com/fifa/app/Entities/Player.java
@@ -1,24 +1,25 @@
 package com.fifa.app.Entities;
 
-import com.fifa.app.Entities.Club;
-import com.fifa.app.Entities.Country;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
-
-@AllArgsConstructor
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Player {
-    private Long id;
-    private String firstName;
-    private String lastName;
+
+    private String id;
+    private String name;
     private LocalDate birthDate;
     private Country country;
     private Club club;
     private int number;
+    private Position position;
+    private Nationality nationality;
 
 }
