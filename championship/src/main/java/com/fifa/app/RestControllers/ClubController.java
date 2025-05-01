354f37fd9e2e4311c575b09ef4dd3e435d8097c3
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-01 15:24:54 +0200

    feat : added a controller for get all

--- championship/src/main/java/com/fifa/app/RestControllers/ClubController.java
+++ championship/src/main/java/com/fifa/app/RestControllers/ClubController.java
@@ -1,11 +1,10 @@
 package com.fifa.app.RestControllers;
 
-import com.fifa.app.DTO.ClubDTO;
+import com.fifa.app.Entities.Club;
 import com.fifa.app.Services.ClubService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +18,8 @@ public class ClubController {
     private final ClubService service;
 
     @GetMapping
-    public ResponseEntity<List<ClubDTO>> getAll(){
-        List<ClubDTO> clubs = service.findAll();
+    public ResponseEntity<List<Club>> getAll() {
+        List<Club> clubs = service.findAll();
         return ResponseEntity.ok(clubs);
-    }
-
-    @GetMapping("{id}")
-    public ResponseEntity<ClubDTO> getById(@PathVariable int id){
-        ClubDTO club = service.findById();
-        return ResponseEntity.ok(club);
     }
 }
