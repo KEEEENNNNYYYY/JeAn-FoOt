ab7932ddc4699e83f5ceb8c00ab3a608f98d10a9
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-01 15:39:33 +0200

    feat : added a controller for crupdate methode of club

--- championship/src/main/java/com/fifa/app/RestControllers/ClubController.java
+++ championship/src/main/java/com/fifa/app/RestControllers/ClubController.java
@@ -4,9 +4,7 @@ import com.fifa.app.Entities.Club;
 import com.fifa.app.Services.ClubService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +18,11 @@ public class ClubController {
     @GetMapping
     public ResponseEntity<List<Club>> getAll() {
         List<Club> clubs = service.findAll();
+        return ResponseEntity.ok(clubs);
+    }
+    @PutMapping
+    public ResponseEntity<List<Club>> createOrUpdateClubs(@RequestBody List<Club> clubs) {
+        service.createOrUpdateClubs(clubs);
         return ResponseEntity.ok(clubs);
     }
 }
