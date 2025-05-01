381899451722ad2c987759c302c25743966ddc3d
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-01 19:56:05 +0200

    fix : DTO attribut

--- championship/src/main/java/com/fifa/app/DTO/PlayerDTO.java
+++ championship/src/main/java/com/fifa/app/DTO/PlayerDTO.java
@@ -6,21 +6,21 @@ import lombok.AllArgsConstructor;
 import com.fifa.app.Entities.Nationality;
 import com.fifa.app.Entities.Position;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class PlayerDTO {
     private String id;
     private String name;
+    private int age;
     private int number;
     private Position position;
     private Nationality nationality;
-    private Club club;
-    private int age;
-
 }
