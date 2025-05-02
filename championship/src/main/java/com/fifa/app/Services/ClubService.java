fb69ab15f8df127af06e04a9c488fcdff9d06d2e
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-02 09:03:55 +0200

    feat : added service for update players

--- championship/src/main/java/com/fifa/app/Services/ClubService.java
+++ championship/src/main/java/com/fifa/app/Services/ClubService.java
@@ -1,12 +1,14 @@
 package com.fifa.app.Services;
 
 import com.fifa.app.DAO.ClubDAO;
+import com.fifa.app.DTO.PlayerDTO;
 import com.fifa.app.Entities.Club;
 import com.fifa.app.Entities.Player;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +21,10 @@ public class ClubService {
     }
     public List<Club> createOrUpdateClubs(List<Club> club) {
         return clubDAO.createOrUpdateClubs(club);
+    }
+
+    public List<PlayerDTO> updateClubPlayers(UUID id, List<PlayerDTO> players) {
+        return clubDAO.updateClubPlayers(id,players);
     }
 
 
