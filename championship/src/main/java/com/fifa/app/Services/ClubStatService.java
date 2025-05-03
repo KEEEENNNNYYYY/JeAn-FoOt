1a90195b8a9a341e428552d85fe7c2ff9925d1ee
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-03 14:06:48 +0200

    feat : added service for club stat

--- championship/src/main/java/com/fifa/app/Services/ClubStatService.java
+++ championship/src/main/java/com/fifa/app/Services/ClubStatService.java
@@ -1,13 +1,20 @@
 package com.fifa.app.Services;
 
+import com.fifa.app.DAO.ClubDAO;
 import com.fifa.app.DTO.ClubStatDTO;
+import com.fifa.app.DTO.ClubStatisticDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ClubStatService {
-    public List<ClubStatDTO> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+    private final ClubDAO statisticDAO;
+
+    public  List<ClubStatisticDTO> findBySeasonYear(int seasonYear, boolean hasToBeClassified){
+        return statisticDAO.findBySeasonYear(seasonYear, hasToBeClassified);
     }
 }
