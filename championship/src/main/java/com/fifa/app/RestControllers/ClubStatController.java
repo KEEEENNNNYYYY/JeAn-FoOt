8670d030ec77228644544015dc900e34867deb64
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-03 14:05:33 +0200

    feat : added endpoint for clubStat

--- championship/src/main/java/com/fifa/app/RestControllers/ClubStatController.java
+++ championship/src/main/java/com/fifa/app/RestControllers/ClubStatController.java
@@ -1,25 +1,28 @@
 package com.fifa.app.RestControllers;
 
 import com.fifa.app.DTO.ClubStatDTO;
+import com.fifa.app.DTO.ClubStatisticDTO;
 import com.fifa.app.Services.ClubStatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/club_stats")
+@RequestMapping("/clubs")
 @RequiredArgsConstructor
 public class ClubStatController {
 
     private final ClubStatService service;
 
-    @GetMapping
-    public ResponseEntity<List<ClubStatDTO>> getAll(){
-        List<ClubStatDTO> stats = service.findAll();
+    @GetMapping("/statistics/{seasonYear}")
+    public ResponseEntity<List<ClubStatisticDTO>> getStatistics(
+        @PathVariable int seasonYear,
+        @RequestParam(defaultValue = "false") boolean hasToBeClassified
+    ) {
+        List<ClubStatisticDTO> stats = service.findBySeasonYear(seasonYear, hasToBeClassified);
         return ResponseEntity.ok(stats);
     }
+
 }
