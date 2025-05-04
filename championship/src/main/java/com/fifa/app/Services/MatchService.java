038e9c4b107538ead169b08bb0d2e8ed73e6ebcb
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-04 08:22:05 +0200

    feat : added service for update status method

--- championship/src/main/java/com/fifa/app/Services/MatchService.java
+++ championship/src/main/java/com/fifa/app/Services/MatchService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +23,10 @@ public class MatchService {
     public List<MatchDisplayDTO> getMatchesFiltered(int seasonYear, String matchStatus, String clubPlayingName,
                                                     LocalDateTime matchAfter, LocalDateTime matchBeforeOrEquals) {
         return matchDAO.findMatchesFiltered(seasonYear, matchStatus, clubPlayingName, matchAfter, matchBeforeOrEquals);
+    }
+
+    public MatchDisplayDTO updateMatchStatus(UUID matchId, String newStatusStr){
+        return matchDAO.updateMatchStatus(matchId, newStatusStr);
     }
 
 }
