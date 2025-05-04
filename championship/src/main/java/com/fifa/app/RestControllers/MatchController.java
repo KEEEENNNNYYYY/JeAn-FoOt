2c120a6a0bafc527db116e18e3157c2deb44a65c
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-04 07:54:53 +0200

    feat : added an endpoint for get match by filter

--- championship/src/main/java/com/fifa/app/RestControllers/MatchController.java
+++ championship/src/main/java/com/fifa/app/RestControllers/MatchController.java
@@ -4,9 +4,12 @@ import com.fifa.app.DTO.MatchDisplayDTO;
 import com.fifa.app.Entities.Match;
 import com.fifa.app.Services.MatchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -27,4 +30,20 @@ public class MatchController {
             return ResponseEntity.status(404).body(e.getMessage());
         }
     }
+
+    @GetMapping("/{seasonYear}")
+    public ResponseEntity<List<MatchDisplayDTO>> getMatchesBySeason(
+        @PathVariable int seasonYear,
+        @RequestParam(required = false) String matchStatus,
+        @RequestParam(required = false) String clubPlayingName,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime matchAfter,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime matchBeforeOrEquals
+    ) {
+        List<MatchDisplayDTO> matches = matchService.getMatchesFiltered(seasonYear, matchStatus, clubPlayingName, matchAfter, matchBeforeOrEquals);
+        if (matches.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(matches);
+        }
+        return ResponseEntity.ok(matches);
+    }
+
 }
