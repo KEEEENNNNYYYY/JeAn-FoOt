c69de90a381ca9c3670a5f2b11ee318b77ecb19d
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-04 08:42:49 +0200

    fix : controller filter

--- championship/src/main/java/com/fifa/app/RestControllers/PlayerController.java
+++ championship/src/main/java/com/fifa/app/RestControllers/PlayerController.java
@@ -1,6 +1,7 @@
 package com.fifa.app.RestControllers;
 
 import com.fifa.app.Entities.Player;
+import com.fifa.app.Entities.PlayerCriteria;
 import com.fifa.app.Services.PlayerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +17,22 @@ public class PlayerController {
     private final PlayerService playerService;
 
     @GetMapping
-    public ResponseEntity<List<Player>> getAll() {
-        List<Player> players = playerService.findAll();
+    public ResponseEntity<List<Player>> getAll(
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) Integer ageMinimum,
+        @RequestParam(required = false) Integer ageMaximum,
+        @RequestParam(required = false) String clubName
+    ) {
+        PlayerCriteria criteria = new PlayerCriteria();
+        criteria.setName(name);
+        criteria.setAgeMinimum(ageMinimum);
+        criteria.setAgeMaximum(ageMaximum);
+        criteria.setClubName(clubName);
+
+        List<Player> players = playerService.findAll(criteria);
         return ResponseEntity.ok(players);
     }
+
 
     @PutMapping
     public ResponseEntity<List<Player>> createOrUpdate(@RequestBody List<Player> players) {
