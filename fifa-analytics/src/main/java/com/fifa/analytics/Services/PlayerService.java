a9b487b675a47f90647366567fed774530f53abd
Ando <hei.ando.14@gmail.com>
2025-05-09 15:44:36 +0300

    feat: round tho 2 decimal places the playingtime

--- fifa-analytics/src/main/java/com/fifa/analytics/Services/PlayerService.java
+++ fifa-analytics/src/main/java/com/fifa/analytics/Services/PlayerService.java
@@ -14,10 +14,13 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -49,15 +52,18 @@ public class PlayerService {
 
     public List<Player> getBestPlayers(Integer top, DurationUnit playingTimeUnit, Integer seasonYear) {
         return playerDAO.getAllPlayers().stream()
+                .filter(player -> player.getPlayerStatistics()
+                        .stream()
+                        .filter(playerStatistics -> Objects.equals(playerStatistics.getSeason(), seasonYear))
+                        .isParallel())
                 .filter(player ->
                         player.getPlayerStatistics() != null &&
                                 !player.getPlayerStatistics().isEmpty() &&
-                            player.getPosition() != Position.GOAL_KEEPER
+                            player.getPosition() != Position.GOAL_KEEPER // to prevent the random value where a GoalKeeper is a superhero when I simulate the data fetching
                         )
                 .map(player -> {
                     PlayerStatistics stats = player.getPlayerStatistics()
                             .stream()
-                            .filter(stat -> stat.getSeason() != null && seasonYear.equals(stat.getSeason()))
                             .peek(playerStatistics -> {
                                 DecimalFormat df = new DecimalFormat();
                                 playerStatistics.getPlayingTime().setDurationUnit(playingTimeUnit);
@@ -68,7 +74,10 @@ public class PlayerService {
                                 if(playingTimeUnit == DurationUnit.HOUR){
                                     time =  time / 3600;
                                 }
-                                Double t = time.doubleValue();
+                                Double t = new BigDecimal(String.valueOf(time))
+                                        .setScale(2, RoundingMode.HALF_UP)
+                                        .doubleValue();
+
                                 playerStatistics.getPlayingTime().setValue(t);
                             })
                             .findFirst()
